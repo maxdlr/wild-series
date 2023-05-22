ec5bb8f1055acae6e77b1e8c1fe8eae14243d2b2
maxdlr <105718327+maxdlr@users.noreply.github.com>
2023-05-22 21:51:37 +0200

    sy quest 5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,13 +6,28 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
+        ]);
+    }
+
+    #[Route(
+        '/{id}/', 
+        requirements: ['id'=>'\d+'], 
+        methods: ['GET'],
+        name: 'item', 
+        )]
+
+    public function show(int $id = 1) :Response
+    {
+        return $this->render('program/item.html.twig', [
+            'id' => $id
         ]);
     }
 }
