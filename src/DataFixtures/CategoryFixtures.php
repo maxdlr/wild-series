2e837fa330ac57bf50f45a0ec24092aad6882e94
maxdlr <105718327+maxdlr@users.noreply.github.com>
2023-05-24 15:44:27 +0200

    sy quest 7

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    static array $categories = [
+        'Action',
+        'Aventure',
+        'Fantastique',
+        'Horreur',
+        'Comedie',
+        'Art Martiaux',
+    ];
+    
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::$categories as $key => $categoryName)
+        {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);  
+        }
+        $manager->flush();          
+    }
+}
