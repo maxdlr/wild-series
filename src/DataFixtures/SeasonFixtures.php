07b8726cf89560c206908edbb7a287b3a6d3b7c7
maxdlr <105718327+maxdlr@users.noreply.github.com>
2023-05-30 22:37:48 +0200

    sy quest 11

--- src/DataFixtures/SeasonFixtures.php
+++ src/DataFixtures/SeasonFixtures.php
@@ -2,19 +2,15 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Season;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Faker;
 
 class SeasonFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $faker = Faker\Factory::create();
-        
-        $seasons = new Season();
-        $manager->persist($seasons);
+        // $product = new Product();
+        // $manager->persist($product);
 
         $manager->flush();
     }
